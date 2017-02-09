360349a50563609678f167822f264889900bc74f
PapaMuziko <ghinzu.iphone@gmail.com>
2017-02-08 19:02:20 -0800

    README

--- w3c_validator.py
+++ w3c_validator.py
@@ -5,8 +5,31 @@ W3C validator for Holberton School
 For HTML and CSS files.
 
 Based on 2 APIs:
+
 - https://validator.w3.org/nu/
 - http://jigsaw.w3.org/css-validator/validator
+
+
+Usage:
+
+Simple file:
+
+```
+./w3c_validator.py index.html
+```
+
+Multiple files:
+
+```
+./w3c_validator.py index.html header.html styles/common.css
+```
+
+All errors are print in `STDERR`
+
+
+References
+
+https://developer.mozilla.org/en-US/
 
 """
 import sys
