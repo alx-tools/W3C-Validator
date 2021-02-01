5ba9e1834a02bc63fd34d394eed84232acb5938d
Juan Marco <jmmarco@gmail.com>
2021-02-01 15:12:43 -0300

    Update API entries and remove references

--- w3c_validator.py
+++ w3c_validator.py
@@ -6,8 +6,8 @@ For HTML and CSS files.
 
 Based on 2 APIs:
 
-- https://validator.w3.org/nu/
-- http://jigsaw.w3.org/css-validator/validator
+- https://validator.w3.org/docs/api.html
+- https://jigsaw.w3.org/css-validator/api.html
 
 
 Usage:
@@ -28,11 +28,6 @@ All errors are printed in `STDERR`
 
 Return:
 Exit status is the # of errors, 0 on Success
-
-References
-
-https://developer.mozilla.org/en-US/
-
 """
 import sys
 import requests
